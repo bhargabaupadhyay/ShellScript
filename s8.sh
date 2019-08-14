774ac13db9a5d0207283f4a6cb7e91d7288b697a
bhargabaupadhyay <170301120058@cutm.ac.in>
2019-08-14 10:21:23 +0530

    user username and password(password is not visible to user typing the password)

---
+++ s8.sh
@@ -0,0 +1,6 @@
+#user username and password(password is not visible to user typing the password)
+
+read -p "username : " user_var
+read -sp "password : " pass_var
+echo "username : " $user_var
+echo "password : " $pass_var
