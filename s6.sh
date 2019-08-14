934ac3aab5b7f14e3dbfcec94479206d8e21ea1d
bhargabaupadhyay <170301120058@cutm.ac.in>
2019-08-14 10:16:47 +0530

    Reading from Keyboard

---
+++ s6.sh
@@ -0,0 +1,11 @@
+#Reading from Keyboard
+echo "Enter Name :"
+read name
+echo "The name is $name"
+echo "Enter Age :"
+read age
+echo "Age is $age"
+echo "Enter salary :"
+read salary
+echo "My salary is $salary"
+
