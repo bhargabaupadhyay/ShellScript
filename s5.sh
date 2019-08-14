64b126d23b50a561ae8daf4a363ca2715bc30bdb
bhargabaupadhyay <170301120058@cutm.ac.in>
2019-08-14 10:13:01 +0530

    Another way to do the arithmetic operation

---
+++ s5.sh
@@ -0,0 +1,9 @@
+# Another way to do the arithmetic operation
+x=20
+y=10
+echo Sum is $(( x + y ))
+echo Difference is $(( x - y ))
+echo Mul value is $(( x * y ))
+echo Div value is $(( x / y ))
+
+
