203c4cc8486a42f4effdefd3f761ed99cf7323b2
bhargabaupadhyay <170301120058@cutm.ac.in>
2019-08-14 10:09:52 +0530

    Aritmetic operation

---
+++ s3.sh
@@ -0,0 +1,9 @@
+#Arithmetic operation 
+
+x=20
+y=10
+
+echo c=`expr $x + $y`
+echo d=`expr $x - $y`
+echo e=`expr $x \* $y`
+echo f=`expr $x / $y`  
---
+++ s4.sh
@@ -0,0 +1,5 @@
+#executing the floating point number
+
+echo "20.5-5" | bc
+echo "20.5*5" | bc
+echo "20.5/5" | bc
